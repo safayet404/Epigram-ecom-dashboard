4f73e0e09d07b010145c351b65ebc13bcea6b216
Safayet Hossain <75946552+safayet404@users.noreply.github.com>
2024-02-10 20:10:22 +0600

    Update App.js
    Hash Router to Browser Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import ResetPassword from "./pages/ResetPassword";
 import MainLayout from "./components/MainLayout";
